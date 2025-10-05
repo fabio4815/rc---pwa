843c9c13d488ea9dcf956862ea92c2f4ecd69440
fabio4815 <fabiobrasil4815@gmail.com>
2025-10-05 13:08:59 -0300

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,8 @@
-const CACHE="rc-cache-v2";const ASSETS=["./","./index.html","./manifest.webmanifest","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting();});
-self.addEventListener("activate",e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE?caches.delete(k):null))));self.clients.claim();});
-self.addEventListener("fetch",e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)));});
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open('rc-cache-v1').then(cache => {
+    return cache.addAll(['./','./index.html','./manifest.webmanifest']);
+  }));
+});
+self.addEventListener('fetch', e => {
+  e.respondWith(caches.match(e.request).then(resp => resp || fetch(e.request)));
+});
